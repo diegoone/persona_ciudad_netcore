beeac2033e192f511b02b7365111f98506cc1798
Diego Palacios <palaciosdiego759@gmail.com>
2021-03-25 10:10:05 -0600

    modificado: olvide agregar el modelo al dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace persona_ciudad.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public virtual DbSet<Ubicacion> Ubicaciones {get;set;}
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- Data/Ubicacion.cs
+++ Data/Ubicacion.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace persona_ciudad.Data
 {
     public class Ubicacion
